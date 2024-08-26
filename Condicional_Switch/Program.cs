4cd7326d7507c833d07be226a375e8cdd5f03334
NetCattt <alejandromurillin@gmail.com>
2024-08-26 14:18:28 -0500

    Se agrego una nueva de linea al codigo y un README.md

--- Condicional_Switch/Program.cs
+++ Condicional_Switch/Program.cs
@@ -3,5 +3,9 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        int numero2 = 32; 
+        double numero3 = 23.4; 
+        Console.Readline("");
     }
 }
